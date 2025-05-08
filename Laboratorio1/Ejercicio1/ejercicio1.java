ce5192e205f23b9a1044af26a116afd59b54f584
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-07 22:01:05 -0500

    recolectando datos

--- Laboratorio1/Ejercicio1/ejercicio1.java
+++ Laboratorio1/Ejercicio1/ejercicio1.java
@@ -1,8 +1,22 @@
 package Ejercicio1;
-
+import java.lang.reflect.Array;
+import java.util.*;;
 public class ejercicio1 {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingresar la cantidad de estudiantes: ");
+        int cantidad = scan.nextInt();
+        ArrayList<Integer> notas = new ArrayList<>();
+        for(int i = 0 ; i < cantidad; i++){
+            System.out.println("Ingresar nota para el estudiante " + (i+1) + ": ");
+            int nota = scan.nextInt();
+            while(nota < 0 || nota > 20){
+                System.out.println("Nota invalida, ingresar nuevamente: ");
+                nota = scan.nextInt();
+            }
+            notas.add(nota);
+        }
+
 
     }
 }
