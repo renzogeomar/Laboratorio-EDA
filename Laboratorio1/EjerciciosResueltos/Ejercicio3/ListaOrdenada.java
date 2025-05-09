6efbf0a70ed83eb7df4c5c58721b5879ac318d0e
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-08 22:14:17 -0500

    copiando el ejercicio 3 de los resueltos

---
+++ Laboratorio1/EjerciciosResueltos/Ejercicio3/ListaOrdenada.java
@@ -0,0 +1,24 @@
+package Laboratorio1.EjerciciosResueltos.Ejercicio3;
+import java.util.*;
+public class ListaOrdenada {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el número de elementos: ");
+        int n = scanner.nextInt();
+        int[] numeros = new int[n];
+        System.out.println("Ingrese los números:");
+        for (int i = 0; i < n; i++) {
+        numeros[i] = scanner.nextInt();
+        }
+        boolean estaOrdenada = true; // Invariante: Se supone que la lista está ordenada
+        for (int i = 1; i < n; i++) {
+        if (numeros[i] < numeros[i - 1]) {
+        estaOrdenada = false; // Se rompe la invariante si encontramos un desorden
+        break;
+        }
+        }
+        System.out.println("¿Está ordenada la lista?: " + (estaOrdenada ? "Sí" : "No"));
+        scanner.close();
+
+    }
+}
