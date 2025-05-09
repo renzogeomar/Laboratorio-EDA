1bd4d51f4935ec9b78d21db83df55f4ee125287d
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-08 21:34:04 -0500

    copiando el ejercicio resuelto 1

--- Laboratorio1/Ejercicio3/ejercicio3.java
+++ Laboratorio1/Ejercicio3/ejercicio3.java
@@ -3,13 +3,20 @@ import java.util.*;
 public class ejercicio3 {
     public static void main(String[] args) {
         Scanner scan= new Scanner(System.in);
-        System.out.println("Ingresar numeros");
+        System.out.println("Ingresar cantidad de numeros");
+        int cantidad= scan.nextInt();
         ArrayList<Integer> numeros= new ArrayList<>();
-        for(int i=0; i<numeros.size(); i++){
+        for(int i=0; i<cantidad; i++){
             int numero= scan.nextInt();
             numeros.add(numero);
         }
+        System.out.println("Lista original:");
+        imprimir(numeros);
         
+        ordenamientoInserccion(numeros);
+
+        System.out.println("Lista ordenada:");
+        imprimir(numeros);  
 
     }
     public static void ordenamientoInserccion(ArrayList<Integer> numeros){
@@ -22,8 +29,10 @@ public class ejercicio3 {
             }
             numeros.set(j+1, key);
         }
-
     }
-
-    
+    public static void imprimir(ArrayList<Integer> numeros){
+        for(int i=0; i<numeros.size(); i++){
+            System.out.println(numeros.get(i));
+        }
+    }
 }
