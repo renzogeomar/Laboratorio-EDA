6d3247a532635a76d03f8200b613f8bf8e9bc237
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-15 23:40:37 -0500

    iniciando ejercicio 3 de los ejercicios propuestos

---
+++ Laboratorio2/EjerciciosPropuestos/ejercicio3/Recursividad.java
@@ -0,0 +1,13 @@
+package Laboratorio2.EjerciciosPropuestos.ejercicio3;
+import java.util.Scanner;
+
+public class Recursividad {
+    public void trianguloRecursivo(int base) {
+        
+
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}    
