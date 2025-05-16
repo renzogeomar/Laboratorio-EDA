b12d3c6583277e0d0ba374b3c40364cf7d02854d
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-15 23:47:13 -0500

    haciendo el metodo para formar el triangulo, funciona pero se muestra de mayor a menor

--- Laboratorio2/EjerciciosPropuestos/ejercicio3/Recursividad.java
+++ Laboratorio2/EjerciciosPropuestos/ejercicio3/Recursividad.java
@@ -1,13 +1,33 @@
 package Laboratorio2.EjerciciosPropuestos.ejercicio3;
 import java.util.Scanner;
-
+/* Triangulo recursivo 1. El método trianguloRecursivo1 calcula y muestra el resultado
+ * Si b = 5
+ * Salida:
+*
+**
+***
+****
+*****
+ */
 public class Recursividad {
     public void trianguloRecursivo(int base) {
-        
+        for(int i=0; i < base; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+        if (base > 1) {
+            trianguloRecursivo(base - 1);
+        }
+
 
     }
     public static void main(String[] args) {
+        Recursividad re = new Recursividad();
         Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese la base del triángulo:");
+        int base = scan.nextInt();
+        System.out.println("Triángulo recursivo:");
+        re.trianguloRecursivo(base);
 
     }
 }    
