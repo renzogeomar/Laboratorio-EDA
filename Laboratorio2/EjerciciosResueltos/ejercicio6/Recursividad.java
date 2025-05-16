ea1ccc9301bca1056ff7955aac4f4be1b4a5ecc4
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-15 21:57:38 -0500

    copiando el ejercicio 6 de los ejercicios resueltos

--- Laboratorio2/EjerciciosResueltos/ejercicio6/Recursividad.java
+++ Laboratorio2/EjerciciosResueltos/ejercicio6/Recursividad.java
@@ -1,11 +1,27 @@
-package Laboratorio2.EjerciciosResueltos;
+package Laboratorio2.EjerciciosResueltos.ejercicio6;
 
-public class ejercicio1{
-    void repetir() {
-        repetir();
+class Recursivdad {
+    static int[] vec = { 312, 614, 88, 22, 54 };
+    void ordenar(int[] v, int cant) {
+        if (cant > 1) {
+            for (int f = 0; f < cant - 1; f++)
+                if (v[f] > v[f + 1]) {
+                    int aux = v[f];
+                    v[f] = v[f + 1];
+                    v[f + 1] = aux;
+                }
+            ordenar(v, cant - 1);
+        }
     }
-
-    public static void main(String[] args) {
-        
+    void imprimir() {
+        for (int f = 0; f < vec.length; f++)
+        System.out.print(vec[f] + " ");
+        System.out.println("\n");
+    }
+    public static void main(String[] ar) {
+        Recursivdad r = new Recursivdad();
+        r.imprimir();
+        r.ordenar(vec, vec.length);
+        r.imprimir();
     }
 }
