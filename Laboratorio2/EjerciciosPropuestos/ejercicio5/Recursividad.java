d25fd5e4a0e545f82f4fe2e9bed8c719e6bf1466
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-16 00:22:29 -0500

    iniciando ejercicio 5 de los ejercicios propuestos, reutilizando codigo del ejercicio anterior

---
+++ Laboratorio2/EjerciciosPropuestos/ejercicio5/Recursividad.java
@@ -0,0 +1,40 @@
+package Laboratorio2.EjerciciosPropuestos.ejercicio5;
+import java.util.Scanner;
+/* Triangulo recursivo 3. El método trianguloRecursivo3 calcula y muestra el resultado
+ * Si b = 5
+ * Salida:
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+
+ */
+public class Recursividad {
+    public void trianguloRecursivo3(int base,int nivel) {
+        if (nivel > base) {
+            return;
+        }
+        // Imprimir espacios, con la ayuda del parametro nivel
+        for (int i = 0; i < base - nivel; i++) {
+            System.out.print(" ");
+        }
+        // Imprimir asteriscos
+        for (int i = 0; i < nivel; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        // Llamada recursiva primero
+        trianguloRecursivo2(base, nivel + 1);
+
+    }
+    public static void main(String[] args) {
+        Recursividad re = new Recursividad();
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Ingrese la base del triángulo: ");
+        int base = scan.nextInt();
+
+        scan.close();
+
+    }
+}    
