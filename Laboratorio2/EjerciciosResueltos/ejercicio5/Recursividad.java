0616cc09215b2015e0e327aeb6bd4eb2e74f1af5
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-15 21:54:45 -0500

    copiando el ejercicio 5 de los ejercicios resueltos

--- Laboratorio2/EjerciciosResueltos/ejercicio5/Recursividad.java
+++ Laboratorio2/EjerciciosResueltos/ejercicio5/Recursividad.java
@@ -1,11 +1,16 @@
-package Laboratorio2.EjerciciosResueltos;
+package Laboratorio2.EjerciciosResueltos.ejercicio5;
 
-public class ejercicio1{
-    void repetir() {
-        repetir();
+public class Recursividad {
+    int factorial(int fact) {
+        if (fact > 0) {
+            int valor = fact * factorial(fact - 1);
+            return valor;
+        } else
+            return 1;
     }
-
-    public static void main(String[] args) {
-        
+    public static void main(String[] ar) {
+        Recursividad re = new Recursividad();
+        int f = re.factorial(4);
+        System.out.println("El factorial de 4 es " + f);
     }
 }
