2baa5b17fa0c8e0b51953854800c05e0a0077bc3
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-15 23:10:22 -0500

    iniciando ejercicio 2 de los ejercicios propuestos

---
+++ Laboratorio2/EjerciciosPropuestos/ejercicio2/Recursividad.java
@@ -0,0 +1,10 @@
+package Laboratorio2.EjerciciosPropuestos.ejercicio2;
+import java.util.Scanner;
+public class Recursividad {
+    public int[] rotarIzquierdaArray(int[] A)
+
+
+    public static void main(String[] args) {
+        
+    }
+}
