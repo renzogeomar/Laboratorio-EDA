f34940363a68a623325905127655f19eeff6f64b
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-15 23:38:27 -0500

    probando el metodo en el main

--- Laboratorio2/EjerciciosPropuestos/ejercicio2/Recursividad.java
+++ Laboratorio2/EjerciciosPropuestos/ejercicio2/Recursividad.java
@@ -11,21 +11,34 @@ public class Recursividad {
         if (d == 0 || A.length == 0) {
         return A;
         }
-
         // Rotar una vez a la izquierda, osea caso base
         int[] rotado = new int[A.length];
         for (int i = 1; i < A.length; i++) {
             rotado[i - 1] = A[i];
         }
         rotado[A.length - 1] = A[0];
-
         // Llamada recursiva hasta que d sea 0, dependera del numero de veces que se quiera rotar
         return rotarIzquierdaArray(rotado, d - 1);
-
     }
 
-
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Recursividad re = new Recursividad();
+        System.out.println("Ingrese el tamaño del arreglo:");
+        int n = scan.nextInt();
+        int[] A = new int[n];
+        System.out.println("Ingrese los " + n + " elementos del arreglo:");
+        for (int i = 0; i < n; i++) {
+            A[i] = scan.nextInt();
+        }
+        System.out.println("Ingrese el número de posiciones a rotar a la izquierda:");
+        int d = scan.nextInt();
+        int[] rotado = re.rotarIzquierdaArray(A, d);
+        System.out.print("Arreglo rotado a la izquierda: ");
+        for (int num : rotado) {
+            System.out.print(num + " ");
+        }
+        scan.close();
         
     }
 }
