05b8d81c8668d21cec28609eb967272320d77302
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-16 21:16:24 -0500

    iniciando ejercicio 6 de los ejercicios propuestos

---
+++ Laboratorio2/EjerciciosPropuestos/ejercicio6/Recursividad.java
@@ -0,0 +1,20 @@
+package Laboratorio2.EjerciciosPropuestos.ejercicio6;
+import java.util.Scanner;
+/* Triangulo recursivo 3. El método trianguloRecursivo3 calcula y muestra el resultado
+ * Si b = 5
+ * Salida:
+*****
+*   *
+*   *
+*   *
+*****
+ */
+public class Recursividad{
+    public void cuadradoRecursivo(){
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
