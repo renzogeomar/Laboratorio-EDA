0127c5178d687b0d66df030947f6d235fc903544
Renzo <rmamaniq@unsa.edu.pe>
2025-05-22 08:08:33 -0500

    probando el ejercicio 1 de los resueltos

--- LAboratorio3/EjercicicosResueltos/Ejercicio1/ejercicio1.java
+++ LAboratorio3/EjercicicosResueltos/Ejercicio1/ejercicio1.java
@@ -1,1 +1,16 @@
 package LAboratorio3.EjercicicosResueltos.Ejercicio1;
+import java.util.ArrayList;
+
+public class ejercicio1{
+    public static void main(String[] args) {
+        ArrayList<String> alumnos = new ArrayList<String>();
+        ArrayList<Integer> notas = new ArrayList<Integer>();
+        alumnos.add("MARIA");
+        alumnos.add("DIEGO");
+        alumnos.add("RENE");
+        alumnos.add("ALONSO");
+        System.out.println(alumnos.hashCode());
+        System.out.println(alumnos.isEmpty());
+        System.out.println(alumnos.size());
+    }
+}
