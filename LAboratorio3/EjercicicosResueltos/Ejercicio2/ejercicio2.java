bde33b3fd2ebd036f0fa92163b17d0f83a24da79
Renzo <rmamaniq@unsa.edu.pe>
2025-05-22 08:15:43 -0500

    probando el ejercicio 2 de los resueltos

--- LAboratorio3/EjercicicosResueltos/Ejercicio2/ejercicio2.java
+++ LAboratorio3/EjercicicosResueltos/Ejercicio2/ejercicio2.java
@@ -1,1 +1,16 @@
 package LAboratorio3.EjercicicosResueltos.Ejercicio2;
+import java.util.ArrayList;
+import java.util.Iterator;
+public class ejercicio2{
+    public static void main(String[] args) {
+        ArrayList<String> alumnos = new ArrayList<String>();
+        alumnos.add("Diego"); 
+        alumnos.add("Pedro");
+        alumnos.add("Juan");
+        alumnos.add("Marta");
+        Iterator<String> itA = alumnos.iterator();
+        while (itA.hasNext()) {
+            System.out.println(itA.next());
+        }
+    }
+}
