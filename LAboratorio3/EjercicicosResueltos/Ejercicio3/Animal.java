a62425b2d41ee00e34c829f3bd510519109a78c9
Renzo <rmamaniq@unsa.edu.pe>
2025-05-22 08:38:02 -0500

    probando el ejercicio 3 de los resueltos

--- LAboratorio3/EjercicicosResueltos/Ejercicio3/Animal.java
+++ LAboratorio3/EjercicicosResueltos/Ejercicio3/Animal.java
@@ -1,1 +1,22 @@
 package LAboratorio3.EjercicicosResueltos.Ejercicio3;
+public class Animal{
+    String nombre;
+    boolean genero;
+    public Animal(String nombre, boolean genero) {
+        super();
+        this.nombre = nombre;
+        this.genero = genero;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public boolean isGenero() {
+        return genero;
+    }
+    public void setGenero(boolean genero) {
+        this.genero = genero;
+    }
+}
--- LAboratorio3/EjercicicosResueltos/Ejercicio3/ejercicio3.java
+++ LAboratorio3/EjercicicosResueltos/Ejercicio3/ejercicio3.java
@@ -1,5 +1,11 @@
 package LAboratorio3.EjercicicosResueltos.Ejercicio3;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ejercicio3 {
-    
+    public static void main(String[] args) {
+        ArrayList<Animal> mascotas = new ArrayList<Animal>();
+        List<Animal> mascotas2 = new List<Animal>(); /** <- Ver error */
+        List<Animal> mascotas3 = new ArrayList<Animal>();
+    }
 }
