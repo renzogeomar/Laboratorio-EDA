1babd1755d98a501971cf46e4cb73a7fb23453d5
Renzo <rmamaniq@unsa.edu.pe>
2025-05-23 11:07:14 -0500

    haciendo el metodo add en la clase list

--- LAboratorio3/EjercicicosPropuestos/Ejercicio1/list.java
+++ LAboratorio3/EjercicicosPropuestos/Ejercicio1/list.java
@@ -1,12 +1,25 @@
 package LAboratorio3.EjercicicosPropuestos.Ejercicio1;
 
 public class list <T> {
-    private node<T> root; //apunta al primer nodo
+    private node<T> root; //apunta al primer nodo/ punto de partida
+    private int size; //tamaño de la lista
     
     public list(){
         this.root = null; //inicializa la lista vacia
     }
     public void add(T data){
-        node<T> newNode = new node<T>(data); // creando un nuevo nodo
+        node<T> newNode = new node<T>(data); //crea un nuevo nodo
+        if (root == null){
+            root = newNode; // crea un nuevo nodo en caso que este vacio
+        }
+        else{
+            node<T> actual = root; //apunta al primer nodo
+            while (actual.getNext() != null){ //recorre la lista hasta el final
+                actual = actual.getNext(); //apunta al siguiente nodo
+            }
+            actual.setNext(newNode); //agrega el nuevo nodo al final de la lista
+        }
     }
+
+
 }
