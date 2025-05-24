a2198d0e11282268f9cba1d65cc3337df8742947
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-23 23:10:23 -0500

    iniciando con la construccion de la clase Operador

---
+++ LAboratorio3/EjercicicosPropuestos/Ejercicio2/Main.java
@@ -0,0 +1,7 @@
+package LAboratorio3.EjercicicosPropuestos.Ejercicio2;
+public class Main{
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ LAboratorio3/EjercicicosPropuestos/Ejercicio2/Operador.java
@@ -0,0 +1,10 @@
+package LAboratorio3.EjercicicosPropuestos.Ejercicio2;
+public class Operador <T extends Number>{
+    private T valor1;
+    private T valor2;
+    public Operador(T valor1, T valor2){
+        this.valor1 = valor1;
+        this.valor2 = valor2;
+    }
+
+}
