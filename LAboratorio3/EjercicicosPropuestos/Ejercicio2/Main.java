4a2cbc7e11bd413b2e81c0b102347dd0d87c3e46
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-23 23:45:49 -0500

    haciendo el metodo division y potencia en la clase main

--- LAboratorio3/EjercicicosPropuestos/Ejercicio2/Main.java
+++ LAboratorio3/EjercicicosPropuestos/Ejercicio2/Main.java
@@ -6,8 +6,18 @@ public class Main{
     static <T extends Number> double resta(T valor1, T valor2){
         return (valor1.doubleValue() - valor2.doubleValue());
     }
-    static <T extends Number> double multiplicacion(T valor1, T valor2){
+    static <T extends Number> double producto(T valor1, T valor2){
         return (valor1.doubleValue() * valor2.doubleValue());
+    }
+    static <T extends Number> double division(T valor1, T valor2) {
+        if (valor2.doubleValue() == 0) {
+            System.out.println("Error: División por cero.");
+            return Double.NaN;
+        }
+        return valor1.doubleValue() / valor2.doubleValue();
+    }
+    static <T extends Number> double potencia(T base, T exponente){
+        return Math.pow(base.doubleValue(), exponente.doubleValue());
     }
     public static void main(String[] args) {
         
