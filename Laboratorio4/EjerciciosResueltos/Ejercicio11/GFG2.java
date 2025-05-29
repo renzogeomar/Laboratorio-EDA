d493b4817e0b31bb92f83a7839b05091ef37099b
Renzo <rmamaniq@unsa.edu.pe>
2025-05-29 13:28:33 -0500

    copiando ejercicio11 de los resueltos

---
+++ Laboratorio4/EjerciciosResueltos/Ejercicio11/GFG2.java
@@ -0,0 +1,15 @@
+package Laboratorio4.EjerciciosResueltos.Ejercicio11;
+
+import java.io.*;
+import java.util.LinkedList;
+
+public class GFG2 {
+    public static void main(String args[]) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("Uno, Dos, Tres ");
+        list.add("Cuatro ");
+
+        // Mostrar el tamaño de la lista
+        System.out.println("El tamaño de la lista es: " + list.size());
+    }
+}
