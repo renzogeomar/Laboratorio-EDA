814315cb4a4779ac68a51065fa5f8b57b4d6979f
Renzo <rmamaniq@unsa.edu.pe>
2025-05-29 13:33:38 -0500

    copiando ejercicio14 de los resueltos

---
+++ Laboratorio4/EjerciciosResueltos/Ejercicio14/LinkedListExample.java
@@ -0,0 +1,27 @@
+package Laboratorio4.EjerciciosResueltos.Ejercicio14;
+
+import java.util.LinkedList;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        // Crea una nueva linked list
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        // Añade elementos a la lista enlazada
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+
+        // Añade un elemento al principio de la lista enlazada
+        linkedList.addFirst(0);
+
+        // Añade un elemento al final de la lista enlazada
+        linkedList.addLast(4);
+
+        // Imprime los elementos de la lista enlazada
+        for (int i : linkedList) {
+            System.out.println(i);
+        }
+    }
+}
+
