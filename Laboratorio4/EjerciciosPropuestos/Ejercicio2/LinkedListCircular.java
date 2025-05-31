b6c24e10d474b523c460f370f12edf00429cc1ca
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-31 17:32:47 -0500

    iniciando con el ejercicio 2 haciendo la clase nodo y la clase LinkedListCircular

---
+++ Laboratorio4/EjerciciosPropuestos/Ejercicio2/LinkedListCircular.java
@@ -0,0 +1,13 @@
+package Laboratorio4.EjerciciosPropuestos.Ejercicio2;
+
+public class LinkedListCircular<E> {
+    private Node<E> head; // Apunta al primer nodo de la lista
+    private Node<E> tail; // Apunta al último nodo de la lista
+    
+    public LinkedListCircular() {
+        this.head = null; // Inicializa la lista vacía
+        this.tail = null; // Inicializa la lista vacía
+    }
+    
+    
+}
