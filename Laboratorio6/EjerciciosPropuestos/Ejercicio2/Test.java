8c5e440d2bd986d7c7c9074eacc87a103e394911
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-14 21:51:21 -0500

    casteando las letras de la palabra para que se conviertan en su equivalente ascii, despues mostrando el arbol que se forma con el metodo printPretty

--- Laboratorio6/EjerciciosPropuestos/Ejercicio2/Test.java
+++ Laboratorio6/EjerciciosPropuestos/Ejercicio2/Test.java
@@ -6,16 +6,20 @@ public class Test{
     public static void main(String[] args){
         BST<Integer> bst = new BST<>();
         ArrayList<Character> lista = new ArrayList<>();
+        ArrayList<Integer> listaASCII = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
         System.out.println("Ingrese una palabra: ");
         String palabra = scan.next();
         for (int i = 0; i < palabra.length(); i++) {
             char letra = palabra.charAt(i);
-            lista.add(letra); // Agrega la letra a la lista    
+            lista.add(letra); // Agrega la letra a la lista
+            int ascii = (int) letra; // Convierte la letra a su valor ASCII
+            listaASCII.add(ascii); // Agrega el valor ASCII a la lista    
+            bst.insert(ascii); // Inserta el valor ASCII en el árbol BST
         }
-        System.out.println("Lista de letras: " + lista);
-
-
-
+        System.out.println("Lista de letras: " + listaASCII);
+        System.out.println("Árbol BST creado con los valores ASCII de las letras.");
+        bst.printPretty();
+        scan.close();
     }
 }
