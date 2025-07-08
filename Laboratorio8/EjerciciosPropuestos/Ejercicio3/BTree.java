d3ce0bfc274a684afbf688b11b9d359afa1119eb
Renzo <rmamaniq@unsa.edu.pe>
2025-07-08 14:41:21 -0500

    completando el metodo search

--- Laboratorio8/EjerciciosPropuestos/Ejercicio3/BTree.java
+++ Laboratorio8/EjerciciosPropuestos/Ejercicio3/BTree.java
@@ -24,5 +24,23 @@ public class BTree<T extends Comparable<T>>{
         return search(root, key) != null;
     }
 
+    private Node<T> search(Node<T> node, T key) {
+        int i = 0;
+        while (i < node.getNumberOfKeys() && key.compareTo(node.getKeys().get(i)) > 0) {
+            i++;
+        }
+        if (i < node.getNumberOfKeys() && key.equals(node.getKeys().get(i))) {
+            return node; // Clave encontrada
+        } 
+        else if (node.isLeaf()) {
+            return null; // Clave no encontrada
+        } 
+        else {
+            return search(node.getChild(i), key); // Buscar en el hijo correspondiente
+        }
+    }
+
+
+
 
 }
